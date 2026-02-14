5e8d5459ddb4f853e55027062e8b2821c62221f2
saibersec <pixel9448@gmail.com>
2026-02-14 12:29:22 +0700

    fix

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ let messages = JSON.parse(fs.readFileSync(FILE));
 let onlineUsers = 0;
 
 io.on("connection", (socket) => {
+
   onlineUsers++;
   io.emit("onlineCount", onlineUsers);
 
-  // kirim semua pesan lama ke user baru
   socket.emit("loadMessages", messages);
 
+  // 🔥 SAAT ADA PESAN BARU
   socket.on("chatMessage", (data) => {
 
     const time = new Date().toLocaleTimeString("id-ID", {
@@ -44,17 +45,26 @@ io.on("connection", (socket) => {
     };
 
     messages.push(newMessage);
-
-    // 🔥 Simpan ke file
     fs.writeFileSync(FILE, JSON.stringify(messages, null, 2));
 
     io.emit("chatMessage", newMessage);
+  });
+
+  // 🔥 TARUH DI SINI (DI BAWAH chatMessage)
+  socket.on("deleteMessage", (id) => {
+
+    messages = messages.filter(msg => msg.id !== id);
+
+    fs.writeFileSync(FILE, JSON.stringify(messages, null, 2));
+
+    io.emit("messageDeleted", id);
   });
 
   socket.on("disconnect", () => {
     onlineUsers--;
     io.emit("onlineCount", onlineUsers);
   });
+
 });
 
 const PORT = process.env.PORT || 3000;
